e6e3c01eea3b8e7ce8fe9531fb931c88dfc6fb6b
Pedro Nilo <pedroniloz.nicola@gmail.com>
2021-07-22 19:16:17 -0300

    Implemented interfaces and classes for MicroRabbit.Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+using System;
+using MicroRabbit.Domain.Core.Events;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        public Command()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime TimeStamp { get; protected set; }
+
+        public Event()
+        {
+            TimeStamp = DateTime.Now;
+        }
+    }
+}
